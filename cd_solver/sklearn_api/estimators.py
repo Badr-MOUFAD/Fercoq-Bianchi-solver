776176eb0712ffbaa318c12b3fba69b28b3d5991
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-11-14 15:30:09 +0100

    sk lasso estimator

---
+++ cd_solver/sklearn_api/estimators.py
@@ -0,0 +1,53 @@
+from cd_solver import Problem, coordinate_descent
+
+
+class Lasso:
+    """Solves un normalized Lasso problem using Fercoq & Bianchi algorithm.
+
+    Objective::
+
+        min_w (1/2) * ||y - Xw||^2 + alpha * ||w||_1
+    """
+
+    def __init__(self, alpha, smooth_formulation=True,
+                 max_iter=1000, verbose=0):
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.verbose = verbose
+        self.smooth_formulation = smooth_formulation
+
+    def fit(self, X, y):
+        alpha = self.alpha
+        n_samples, n_features = X.shape
+
+        if self.smooth_formulation:
+            datafit_params = {
+                'f': ["square"], 'Af': X, 'bf': y,
+                'blocks_f': [0, n_samples],
+                'cf': [0.5],
+            }
+        else:
+            datafit_params = {
+                'h': ["square"], 'Ah': X, 'bh': y,
+                'blocks_h': [0, n_samples],
+                'ch': [0.5],
+            }
+
+        # cf. file comparison.py
+        # examples leukemia on Lasso
+        pb = Problem(
+            N=n_features,
+            # datafit
+            ** datafit_params,
+            # penalty
+            g=["abs"] * n_features, cg=[alpha] * n_features
+        )
+        coordinate_descent(pb, max_iter=self.max_iter,
+                           verbose=self.verbose, per_pass=1)
+
+        self.coef_ = pb.sol
+        return self
+
+
+if __name__ == '__main__':
+    pass
